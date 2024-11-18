a57d1948778fc12f4562b89c11e28586c4e97030
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-11-17 21:15:58 -0800

    compare discarded

--- lab2framework/agents/osawa.py
+++ lab2framework/agents/osawa.py
@@ -44,27 +44,79 @@ class OuterStatePlayer(agent.Agent):
     def __init__(self, name, pnr):
         self.name = name
         self.hints = {}
-        self.pnr = pnr
+        self.pnr = pnr #player number
         self.explanation = []
 
     #utilize trash and played for process of elimination for more information of hand
     def get_action(self, nr, hands, knowledge, trash, played, board, valid_actions, hints, hits, cards_left):
+        #def update_Knowledge(hands, played, discarded, my_knowledge, nr):
 
+        def adjust_card_count(card_map, color, rank, delta):
+            if color in card_map and rank in card_map[color]:
+                card_map[color][rank] += delta
+                # Ensure the count doesn't go below 0
+                if card_map[color][rank] < 0:
+                    card_map[color][rank] = 0
+            else:
+                raise ValueError("Invalid color or rank")
+            
+        card_map = {
+            color: {rank: COUNTS[rank] for rank in range(len(COUNTS))}
+            for color in range(len(ALL_COLORS))
+        }
+
+        for _,card in enumerate(trash):
+            adjust_card_count(card_map, card.color, card.rank - 1, -1)
+            print(card)
+#################################################################
+        # for _, card in enumerate(played):
+        #     adjust_card_count(card_map, card.color, card.rank - 1, -1)
+
+        # for i, hand in enumerate(hands):
+        #         #if(i == nr):
+        #             #continue
+        #         for _, card in enumerate(hand):
+        #             adjust_card_count(card_map, card.color, card.rank - 1, -1)
+        
+        # total_cards = 0
+        # for color, ranks in card_map.items():
+        #     # Sum up the counts for all ranks under this color
+        #     total_cards += sum(ranks)
+        # print("out cards", len(trash) + len(played))
+        # print("Trash Cards:", [(card.color, card.rank) for card in trash])
+        # print("Played Cards:", [(card.color, card.rank) for card in played])
+        # print("Other Hands Cards:", [(card.color, card.rank) for hand in hands for card in hand if hand != hands[nr]])
+
+        #print(card_map)
+        #print("total cards", total_cards)
+        #print("-----------------------------------")
+        # print(trash)
+#################################################################
+        #hands of other players
         for player,hand in enumerate(hands):
-            for card_index,_ in enumerate(hand):
+            for card_index,card in enumerate(hand):
+                #if the card in a players hand does not have any hints, create an empty set of hints for it
                 if (player,card_index) not in self.hints:
-                    self.hints[(player,card_index)] = set()
+                    self.hints[(player,card_index)] = set() #players hints about card at index card_index = set()
         known = [""]*5
         for h in self.hints:
             pnr, card_index = h 
-            if pnr != nr:
+            if pnr != nr: #if player number is not the current player
                 known[card_index] = str(list(map(format_hint, self.hints[h])))
         self.explanation = [["hints received:"] + known]
 
         #your hand
         my_knowledge = knowledge[nr]
-        
-        #goes through your theoretical hand and checks if something can be played while also checking and adding useless cards into an array
+        print(card_map)
+
+        for i,k in enumerate(my_knowledge):
+            print(k)
+            print("---------------------------", "\n")
+            #for j,card in enumerate(my_knowledge[i]):
+                # print(card, "\n")
+                # print("---------------------------", "\n")
+
+        #goes through your potential hand and checks if something can be played while also checking and adding useless cards into an array
         potential_discards = []
         for i,k in enumerate(my_knowledge):
             if util.is_playable(k, board):
@@ -140,6 +192,7 @@ class OuterStatePlayer(agent.Agent):
                 if (player,action.card_index+i+1) in self.hints:
                     self.hints[(player,action.card_index+i)] = self.hints[(player,action.card_index+i+1)]
                     self.hints[(player,action.card_index+i+1)] = set()
+
 
 agent.register("inner", "Inner State Player", InnerStatePlayer)
 agent.register("outer", "Outer State Player", OuterStatePlayer)
